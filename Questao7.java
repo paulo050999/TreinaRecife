b5b6ad6142497fc54111f61b83d17aeaa38e9271
ThaisaGV <thaisa_vilaca@hotmail.com>
2025-01-06 18:02:08 -0300

    atualização

--- Questao7.java
+++ Questao7.java
@@ -11,7 +11,7 @@ public class Questao7 {
         System.out.println("Digite o comprimento do lado do quadrado");
         double l = scanner.nextDouble();
         double area = l*l;
-        System.out.println("O área do quadrado é  " + area);
+        System.out.println("O área do quadrado é  " + area*2);
         scanner.close();
 }
 }
