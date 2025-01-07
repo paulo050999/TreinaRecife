3d410a0d458a52c1383504f8f1ef588e4b4746fb
ThaisaGV <thaisa_vilaca@hotmail.com>
2025-01-07 17:35:16 -0300

    07/01

---
+++ poo/POO01ES01.java
@@ -0,0 +1,7 @@
+// Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+public class POO01ES01 {
+    public static void main(String []args){
+        System.out.println("Alo mundo");
+    }
+}
